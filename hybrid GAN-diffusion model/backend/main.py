72e6f81347df94e942b72b766bce8ca3b455cbb4
Beda3113 <130255340+Beda3113@users.noreply.github.com>
2025-06-15 13:29:35 +0300

    Update main.py

--- hybrid GAN-diffusion model/backend/main.py
+++ hybrid GAN-diffusion model/backend/main.py
@@ -1,8 +1,21 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from api.logo_router import router as logo_router
+from shared.config import RESULT_DIR, LORA_PATH
 
 app = FastAPI()
+
+# CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+# Подключаем маршруты
 app.include_router(logo_router, prefix="/api")
 
 if __name__ == "__main__":
